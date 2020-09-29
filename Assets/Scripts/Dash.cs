55e0a937b28891f42f6092d442b8ab5f22ff5554
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2020-09-29 16:42:17 -0500

    Movement now uses physics, character prefab

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+	private Rigidbody rb;
+	private bool dash;
+	private float crDash;
+
+	public float boostSpeed = 2;
+	public float dashTimeout = 0.5f;
+	public float dashTime = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+		crDash = dashTimeout;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(dashTimeout <= crDash) {
+			if(Input.GetKeyDown(SpacebarKey())) {
+				crDash = 0;
+				dash = true;
+			}
+		} else {
+			crDash += Time.deltaTime;
+
+			if(dashTime < crDash) {
+				dash = false;
+			}
+		}
+    }
+
+	void LateUpdate() {
+		if(dash) {
+			rb.velocity = rb.velocity * boostSpeed;
+		}
+    }
+
+	// In linux there's a bug that in editor mode spacebar is detected as an O
+	// This kinda solves this issue
+	public static KeyCode SpacebarKey() {
+	if (Application.isEditor) return KeyCode.O;
+	else return KeyCode.Space;
+	}
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	public float speed = 15;
-	public float boostSpeed = 30;
+	public float speed = 7;
+	private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start() {
+		rb = GetComponent<Rigidbody>();
 	}
 
     // Update is called once per frame
     void Update() {
 		float vertical = 0;
 		float horizontal = 0;
-		float boost = 1;
 
 		// Get each movement key
 		if(Input.GetKey(KeyCode.W)) {
@@ -31,21 +31,11 @@ public class Movement : MonoBehaviour {
 			horizontal -= 1;
 		}
 
-		if(Input.GetKeyDown(SpacebarKey())) {
-			boost = boostSpeed;
-		}
-
 		// Make movement vector
-		Vector3 movement = new Vector3(horizontal,0,vertical);
-		movement = Quaternion.Euler(0,45,0) * movement;
+		Vector3 movement = new Vector3(horizontal,0.0f,vertical);
 
-		transform.Translate(movement * boost * speed * Time.deltaTime);
+		// rb.AddForce(movement * speed);
+		rb.velocity = movement * speed;
     }
 
-	// In linux there's a bug that in editor mode spacebar is detected as an O
-	// This kinda solves this issue
-	public static KeyCode SpacebarKey() {
-	if (Application.isEditor) return KeyCode.O;
-	else return KeyCode.Space;
-	}
 }
